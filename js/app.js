f25c81e9e6f41f2fcf0f5e1b839aa0a4f5b397f9
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2018-06-26 20:07:30 +0200

    style: improved comments

--- js/app.js
+++ js/app.js
@@ -14,24 +14,26 @@ const repeat = document.querySelector(".fa-repeat"); // gets the repeat icon
 const openedCards=[]; //array to check for match
 const matchedCards=[]; //array to store matched cards
 let myTimeOut; //assigened by timeOut() line 127
-let counter=0;
-const moveC= document.querySelector(".moves");
-const modal = document.querySelector(".modal");
+let counter=0;//stores mouse clicks on cards 
+const moveC= document.querySelector(".moves"); //used to update the move counter html 
+const modal = document.querySelector(".modal");//used to apply the class the makes the modal window visble
 const closeButton = document.querySelector(".close-modal");
-const tMinutes= document.querySelector(".minutes");
+const tMinutes= document.querySelector(".minutes");//updates the minutes counter html
 const tSeconds=document.querySelector(".seconds");
-let minTimer=0;
-let secTimer=0;
-let secInterval;
-const stars=document.querySelectorAll(".fa-star");
+let minTimer=0;//keeps track of minutes
+let secTimer=0;//keeps track of seconds
+let secInterval;//sotres the interval function used for timer
+const stars=document.querySelectorAll(".fa-star");//used to acces the stars
 const starOne=stars[0];
 const starTwo=stars[1];
 const starThree=stars[2];
-const starsScore = document.querySelector(".stars-score");
+const starsScore = document.querySelector(".stars-score");//to update the modal window html
 const minComplete= document.querySelector(".min-complete");
 const secComplete= document.querySelector(".sec-complete");
-const playAgain = document.querySelector(".play-again");/*/*
-Functions 
+const playAgain = document.querySelector(".play-again");
+
+/*
+*Functions 
 */ 
 
 /*
@@ -64,18 +66,18 @@ function buildDeck(){
 */
 //function called when restart is clicked
 function restart(){ 
-    stopGameTimer();//stops game timer (see line 222)
-    clearScore(); //clear all the score and time counters (line 77)
+    stopGameTimer();//stops game timer (see line 242)
+    clearScore(); //clear all the score and time counters (line 80)
     if(matchedCards.length>0){
-        resetCards(matchedCards);//resets the matchedCards[](line 90)
+        resetCards(matchedCards);//resets the matchedCards[](line 94)
     }
     if(openedCards.length>0){
-        resetCards(openedCards);// resets the openedCards[](line 90)
+        resetCards(openedCards);// resets the openedCards[](line 94)
     }
-    buildDeck(); //builds the deck (line 55)
+    buildDeck(); //builds the deck (line 56)
 }
 //clears score and timer
-function clearScore(){
+function clearScore(){//called by restart() line 68
     minTimer=0;
     secTimer=0;
     counter=0;
@@ -88,10 +90,10 @@ function clearScore(){
         }
     }
 }
-//splices the given array by the number of cards indicated
-function resetCards(array){
-    closeCards(array);
-    clearArray(array, array.length);
+//splices the given array b
+function resetCards(array){ //called by restart() line 68
+    closeCards(array); //see line 167
+    clearArray(array, array.length); //see line 163
 }
 /*
 this section deals with flipping cards
@@ -99,32 +101,32 @@ this section deals with flipping cards
 //function called from the event listener, it uses the event object to leverage event delegation
 function flipCards(e) { 
     if(counter==0){
-        gameTimer();//starts the game timer (line 201)
+        gameTimer();//starts the game timer (line 218)
     } 
     if (e.target !== e.currentTarget && openedCards.length<2) { //first condition keeps the event from being trigered by parent element (ul) second one keeps user from turning more than 2 cards
         let clickedItem = e.target; //gets the event target
-        openShow(clickedItem); //flips cards (line 132)
-        pushToArray(clickedItem); //pushes to the array used to check for match (line 140)
+        openShow(clickedItem); //flips cards (line 137)
+        pushToArray(clickedItem); //pushes to the array used to check for match (line 145)
         if(openedCards.length==2){
-            checkMatch(); // check if its a match, if it is, send cards to a new array (line 146)           
-            timeOut(); //sets a 1000 mms timer to close opened cards (line 121)
+            checkMatch(); // check if its a match, if it is, send cards to a new array (line 151)           
+            timeOut(); //sets a 1000 mms timer to close opened cards (line 125)
             return timeOut; //returns setTimeout() to be able to use clearTimeOut()
         }
     }else if(openedCards.length==2 && e.target !== e.currentTarget){
-        stopTimeout(); //clears the timeout (line 128)
-        closeCards(openedCards); //closes the opened cards (line 162)
-        clearArray(openedCards, openedCards.length);//clears the opened cards array (line 158)
-        openShow(e.target);//opens the clicked card (line 132)
-        pushToArray(e.target);//pushes the clicked card to the openedCards[](line 140)
+        stopTimeout(); //clears the timeout (line 133)
+        closeCards(openedCards); //closes the opened cards (line 167)
+        clearArray(openedCards, openedCards.length);//clears the opened cards array (line 163)
+        openShow(e.target);//opens the clicked card (line 137)
+        pushToArray(e.target);//pushes the clicked card to the openedCards[](line 145)
     }
     e.stopPropagation(); //keeps the event from propagating (bubling) past the clicked item
 }
 // waits .5 sec and then clear the cards
 function timeOut(){
     myTimeOut = setTimeout(function(){
-        closeCards(openedCards);//(line 162)
+        closeCards(openedCards);//(line 167)
         clearArray(openedCards, openedCards.length);
-    }, 500);//(line 158)
+    }, 500);//(line 163)
     return myTimeOut;
 }
 //stops the timeout
@@ -132,21 +134,21 @@ function stopTimeout(){
     clearTimeout(myTimeOut);
 }
 //opens cards
-function openShow(clickedItem){ //adds/removes the open and show classes
+function openShow(clickedItem){ //adds/removes the open and show classes, called by flipCards line 102
     counter++;
     clickedItem.classList.toggle("open");
     clickedItem.classList.toggle("show");
     clickedItem.classList.toggle("stop-click");
     moveC.innerHTML=counter;
 }
-// pushes opend card to array
-function pushToArray(clickedItem){ //it adds opened cards to an empty array
+// pushes opened card to array
+function pushToArray(clickedItem){ //it adds opened cards to an empty array, called by flipCards line 102
     if(clickedItem.getAttribute("class")=="card open show stop-click"){ 
         openedCards.push(clickedItem); //pushes to given array array
     }   
 }
 //checks for match
-function checkMatch(){ //checks the array that holds the opened cards for a match
+function checkMatch(){ //checks the array that holds the opened cards for a match, called by flipCards line 102
     let cardOne=openedCards[0].children[0].getAttribute("class"); //gets the class of the i elelements present in the openedCards array
     let cardTwo=openedCards[1].children[0].getAttribute("class");
     if(cardOne==cardTwo){ //checks if they have the same class
@@ -157,7 +159,7 @@ function checkMatch(){ //checks the array that holds the opened cards for a matc
         victory(); //asses the victory condition and if they apply it stops timer and calls modal window(line 186)
     }
 }
-//clears the array
+//clears arrays
 function clearArray(arrayName, cardsToErase){
     arrayName.splice(0, cardsToErase); //clears the array
 }
